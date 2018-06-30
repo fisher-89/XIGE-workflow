ce6ff6f44c049e83401a816582a5fa61b9382c42
1032380143 <1032380143@qq.com>
2018-06-30 11:25:31 +0800

    添加表单分类模型观察器

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\FormType;
+use App\Observers\TypeObserver;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
@@ -26,7 +28,7 @@ class EventServiceProvider extends ServiceProvider
     public function boot()
     {
         parent::boot();
-
+        FormType::observe(TypeObserver::class);//表单分类缓存处理
         //
     }
 }
