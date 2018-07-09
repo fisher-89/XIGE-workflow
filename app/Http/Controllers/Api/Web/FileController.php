c2c282369d8508a061144d10f0aab829b2192ecb
1032380143 <1032380143@qq.com>
2018-07-09 11:38:17 +0800

    前台：文件上传目录移动

--- app/Http/Controllers/Api/Web/FileController.php
+++ app/Http/Controllers/Api/Web/FileController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api\Web;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
