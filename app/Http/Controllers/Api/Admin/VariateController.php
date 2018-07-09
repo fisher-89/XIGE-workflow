42283518794d75b074d0ac30d9d185fb6a79604d
1032380143 <1032380143@qq.com>
2018-07-09 11:05:05 +0800

    后台：计算与变量获取修改方式

--- app/Http/Controllers/Api/Admin/VariateController.php
+++ app/Http/Controllers/Api/Admin/VariateController.php
@@ -2,25 +2,34 @@
 
 namespace App\Http\Controllers\Api\Admin;
 
-use App\Models\DefaultValueCalculation;
-use App\Models\DefaultValueVariate;
+
+use App\Services\DefaultValueCalculationService;
+use App\Services\DefaultValueVariateService;
 use App\Services\ResponseService;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class VariateController extends Controller
 {
+
+    protected $variate;
+    protected $calculation;
+
+    public function __construct(DefaultValueVariateService $defaultValueVariateService, DefaultValueCalculationService $defaultValueCalculationService)
+    {
+        $this->variate = $defaultValueVariateService;
+        $this->calculation = $defaultValueCalculationService;
+    }
+
     /**
      * 获取默认值变量与计算公式数据
      * @return mixed
      * @throws \Illuminate\Container\EntryNotFoundException
      */
-    public function index(ResponseService $responseService){
-        $variateData = DefaultValueVariate::get()->toArray();
-        $calculationData = DefaultValueCalculation::get()->toArray();
-        $data =[
-            'variate'=>$variateData,
-            'calculation'=>$calculationData
+    public function index(ResponseService $responseService)
+    {
+        $data = [
+            'variate' => array_values($this->variate->get()),
+            'calculation' => array_values($this->calculation->get()),
         ];
         return $responseService->get($data);
     }
