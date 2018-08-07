a11c549fdc4e910580ef32d1173d7ab757036c71
1032380143 <1032380143@qq.com>
2018-08-07 13:52:05 +0800

    流程模型去除create_at、update_at字段

--- app/Models/Flow.php
+++ app/Models/Flow.php
@@ -9,7 +9,7 @@ class Flow extends Model
 {
     use SoftDeletes;
     protected $fillable = ['name', 'description', 'flow_type_id', 'form_id', 'sort', 'is_active', 'start_callback_uri', 'end_callback_uri'];
-    protected $hidden = ['created_at','updated_at','deleted_at'];
+    protected $hidden = ['deleted_at'];
     protected $appends = ['flows_has_staff', 'flows_has_roles', 'flows_has_departments'];
 
     public function form()
