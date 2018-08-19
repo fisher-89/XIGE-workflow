6950bd9ef58654e11ca539e9786a0ee2a4826f22
1032380143 <1032380143@qq.com>
2018-08-19 10:11:50 +0800

    step_run表添加待办事项唯一id字段

--- app/Models/StepRun.php
+++ app/Models/StepRun.php
@@ -11,7 +11,7 @@ class StepRun extends Model
 
     protected $table = 'step_run';
     protected $hidden = ['updated_at', 'deleted_at'];
-    protected $fillable = ['step_id', 'step_key','step_name','flow_type_id', 'flow_id', 'flow_name', 'flow_run_id', 'form_id', 'data_id', 'approver_sn', 'approver_name', 'checked_at', 'action_type', 'acted_at', 'remark'];
+    protected $fillable = ['step_id', 'step_key','step_name','flow_type_id', 'flow_id', 'flow_name', 'flow_run_id', 'form_id', 'data_id', 'approver_sn', 'approver_name', 'checked_at', 'action_type', 'acted_at', 'remark','record'];
 
     public function steps(){
         return $this->belongsTo('App\Models\Step','step_id');
