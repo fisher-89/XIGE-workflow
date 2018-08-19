e19af78b2503413b3ca460f761e2f71e2cb5099e
1032380143 <1032380143@qq.com>
2018-08-19 10:17:44 +0800

    step_run表添加待办事项唯一id字段

--- database/migrations/2018_03_09_092212_create_workflow_table.php
+++ database/migrations/2018_03_09_092212_create_workflow_table.php
@@ -222,7 +222,7 @@ class CreateWorkflowTable extends Migration
             $table->char('remark', 200)->comment('操作备注')->default('');
             $table->unsignedTinyInteger('is_rejected')->default(0)->comment('上一步为驳回 1是 0否');
             $table->char('next_id',20)->default('')->comment('下一步id');
-            $table->string('record')->nullable()->comment('钉钉 待办事项唯一id，更新待办事项的时候需要用到');
+            $table->string('record_id')->nullable()->comment('钉钉 待办事项唯一id，更新待办事项的时候需要用到');
             $table->timestamps();
             $table->softDeletes();
             $table->index('step_key');
