89358e73f7c6757ce68bf0f0e559194c8b0474ab
1032380143 <1032380143@qq.com>
2018-09-05 17:28:55 +0800

    迁移文件：表单字段地区联动添加详细地址

--- database/migrations/2018_03_09_092212_create_workflow_table.php
+++ database/migrations/2018_03_09_092212_create_workflow_table.php
@@ -60,7 +60,7 @@ class CreateWorkflowTable extends Migration
                 ->comment("字段类型 'int'数字, 'text'文本, 'date'日期, 'datetime'日期时间, 'time'时间, 'array'数组,'select'选择器, 'file'文件,'department'部门控件，'staff'员工控件，'shop'店铺控件,'region'地区 ");
             $table->unsignedTinyInteger('is_checkbox')->default(0)->comment('控件是否多选 0否 1是');
             $table->text('condition')->nullable()->comment('控件条件');
-            $table->unsignedTinyInteger('region_level')->nullable()->comment('地区联动级数 1，2，3');
+            $table->unsignedTinyInteger('region_level')->nullable()->comment('地区联动级数 1，2，3,4');
             $table->char('min', 20)->comment('最小值 可填数字、日期、today等')->default('');
             $table->char('max', 20)->comment('最大值')->default('');
             $table->unsignedTinyInteger('scale')->comment('小数位数')->default(0);
