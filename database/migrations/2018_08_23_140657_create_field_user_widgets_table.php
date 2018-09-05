4ae6f9faf7a0a65342cb678041534d109ab833e1
1032380143 <1032380143@qq.com>
2018-09-05 15:23:58 +0800

    迁移文件：员工、部门、店铺控件字段优化

--- database/migrations/2018_08_23_140657_create_field_user_widgets_table.php
+++ database/migrations/2018_08_23_140657_create_field_user_widgets_table.php
@@ -16,7 +16,8 @@ class CreateFieldUserWidgetsTable extends Migration
         Schema::create('field_user_widgets', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('field_id')->comment('字段ID')->index();
-            $table->char('oa_id','20')->comment('员工ID、部门ID、店铺ID');
+            $table->char('value',20)->comment('员工ID、部门ID、店铺ID');
+            $table->char('text',100)->comment('员工名字、部门名称、店铺名称');
 //            $table->timestamps();
         });
     }
