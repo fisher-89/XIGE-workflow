5656cdd526c5c80d7873bcfae78adb6668f510ed
1032380143 <1032380143@qq.com>
2018-09-11 08:56:31 +0800

    前台：预提交验证优化

--- app/Http/Requests/Web/PresetRequest.php
+++ app/Http/Requests/Web/PresetRequest.php
@@ -46,17 +46,51 @@ class PresetRequest extends FormRequest
             ],
         ];
         $fieldsRules = app('validation')->makeStepFormValidationRules($this->step);
-
         return array_collapse([$basicRules, $fieldsRules]);
     }
 
     public function attributes()
     {
         $fieldAttributes = $this->step->flow->form->fields->mapWithKeys(function ($field) {
+            $msg  =[];
             if ($field->grid) {
-                return ['form_data.' . $field->grid->key . '.*.' . $field->key => $field->name, 'form_data.' . $field->grid->key => $field->grid->name];
+                $msg =  ['form_data.' . $field->grid->key . '.*.' . $field->key => $field->name, 'form_data.' . $field->grid->key => $field->grid->name];
+                switch($field->type){
+                    case 'region':
+                        $msg['form_data.'.$field->grid->key.'.*.'.$field->key.'.province_id'] = $field->name.'的省';
+                        $msg['form_data.'.$field->grid->key.'.*.'.$field->key.'.city_id'] = $field->name.'的市';
+                        $msg['form_data.'.$field->grid->key.'.*.'.$field->key.'.county_id'] = $field->name.'的区、县';
+                        $msg['form_data.'.$field->grid->key.'.*.'.$field->key.'.address'] = $field->name.'的详情地址';
+                        break;
+                    case 'staff':
+                        if($field->is_checkbox == 0){
+                            $msg['form_data.'.$field->grid->key.'.*.'.$field->key.'.value'] = $field->name.'的员工';
+                        }else{
+                            $msg['form_data.'.$field->grid->key.'.*.'.$field->key.'.*.value'] = $field->name.'的员工';
+                        }
+                        break;
+                    case 'department':
+                        if($field->is_checkbox == 0){
+                            $msg['form_data.'.$field->grid->key.'.*.'.$field->key.'.value'] = $field->name.'的部门';
+                        }else{
+                            $msg['form_data.'.$field->grid->key.'.*.'.$field->key.'.*.value'] = $field->name.'的部门';
+                        }
+                        break;
+                    case 'shop':
+                        if($field->is_checkbox == 0){
+                            $msg['form_data.'.$field->grid->key.'.*.'.$field->key.'.value'] = $field->name.'的店铺';
+                        }else{
+                            $msg['form_data.'.$field->grid->key.'.*.'.$field->key.'.*.value'] = $field->name.'的店铺';
+                        }
+                        break;
+                }
             }
-            return ['form_data.' . $field->key => $field->name];
+            $msg['form_data.' . $field->key ] =  $field->name;
+            $msg['form_data.' . $field->key.'.province_id' ] =  $field->name.'的省';
+            $msg['form_data.' . $field->key.'.city_id' ] =  $field->name.'的市';
+            $msg['form_data.' . $field->key.'.county_id' ] =  $field->name.'的区、县';
+            $msg['form_data.' . $field->key.'.address' ] =  $field->name.'的详情地址';
+            return $msg;
         })->toArray();
         return array_collapse([$fieldAttributes, [
             'step_run_id' => '步骤运行ID',
--- app/Services/Web/ValidationService.php
+++ app/Services/Web/ValidationService.php
@@ -9,6 +9,8 @@
 namespace App\Services\Web;
 
 
+use Illuminate\Validation\Rule;
+
 class ValidationService
 {
     /**
@@ -25,6 +27,7 @@ class ValidationService
         $requiredFields = $step->required_fields;
         $rules = [];
         $fields->each(function ($field) use (&$rules, $editableFields, $requiredFields) {
+            //字段key
             $key = $field->grid ? $field->grid->key . '.*.' . $field->key : $field->key;
             if (in_array($key, $editableFields)) {
                 $fieldRules = $field->validator->map(function ($validator) {
@@ -43,8 +46,11 @@ class ValidationService
                         $fieldRules[] = 'string';
                         break;
                     case 'file':
-//                        $fieldRules[] = 'file';
                         $fieldRules[] = 'array';
+                        break;
+                    case 'select':
+                        $rules['form_data.' . $key . '.*'] = $fieldRules;
+                        $fieldRules = ['array'];
                         break;
                     case 'array':
                         $rules['form_data.' . $key . '.*'] = $fieldRules;
@@ -58,6 +64,59 @@ class ValidationService
                         break;
                     case 'time':
                         $fieldRules[] = 'date_format:"H:i:s"';
+                        break;
+                    case 'region':
+                        $region = request()->get('form_data')[$key];
+                        $rules['form_data.' . $key . '.province_id'] = [
+                            'nullable',
+                            Rule::exists('region', 'id')->where('level', 1)
+                        ];
+                        $rules['form_data.' . $key . '.city_id'] = [
+                            'nullable',
+                            Rule::exists('region', 'id')->where('level', 2)->where('parent_id', $region['province_id'])
+                        ];
+                        $rules['form_data.' . $key . '.county_id'] = [
+                            'nullable',
+                            Rule::exists('region', 'id')->where('level', 3)->where('parent_id', $region['city_id'])
+                        ];
+                        $rules['form_data.' . $key . '.address'] = [
+                            'nullable',
+                            'string'
+                        ];
+                        $fieldRules[] = 'array';
+                        break;
+                    case 'staff':
+                        if (count($field->widgets)>0) {
+                            $staffSns = $field->widgets->pluck('value')->all();
+                            if ($field->is_checkbox == 0) {
+                                $rules['form_data.' . $key . '.value'] = 'in:' . implode(',', $staffSns);
+                            } else {
+                                $rules['form_data.' . $key . '.*.value'] = 'in:' . implode(',', $staffSns);
+                            }
+                        }
+                        $fieldRules[] = 'array';
+                        break;
+                    case 'department':
+                        if (count($field->widgets)>0) {
+                            $departmentIds = $field->widgets->pluck('value')->all();
+                            if ($field->is_checkbox == 0) {
+                                $rules['form_data.' . $key . '.value'] = 'in:' . implode(',', $departmentIds);
+                            } else {
+                                $rules['form_data.' . $key . '.*.value'] = 'in:' . implode(',', $departmentIds);
+                            }
+                        }
+                        $fieldRules[] = 'array';
+                        break;
+                    case 'shop':
+                        if (count($field->widgets)>0) {
+                            $shopIds = $field->widgets->pluck('value')->all();
+                            if ($field->is_checkbox == 0) {
+                                $rules['form_data.' . $key . '.value'] = 'in:' . implode(',', $shopIds);
+                            } else {
+                                $rules['form_data.' . $key . '.*.value'] = 'in:' . implode(',', $shopIds);
+                            }
+                        }
+                        $fieldRules[] = 'array';
                         break;
                 }
 
@@ -84,4 +143,5 @@ class ValidationService
             if ($field->min) $fieldRules[] = 'min:' . $field->min;
         }
     }
+
 }
