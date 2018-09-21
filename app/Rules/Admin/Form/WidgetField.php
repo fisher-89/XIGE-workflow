607e4d6b9f53bef477398a082eb3c63fe9c31db5
1032380143 <1032380143@qq.com>
2018-09-21 15:22:40 +0800

    后台：字段可选项验证优化

--- app/Rules/Admin/Form/WidgetField.php
+++ app/Rules/Admin/Form/WidgetField.php
@@ -33,12 +33,17 @@ class WidgetField implements Rule
                 $this->msg = '默认值 不在可选项里';
                 return false;
             }
-            if($value && $this->field['max'] && count($value)> $this->field['max']){
+            if ($value && $this->field['max'] && count($value) > $this->field['max']) {
                 $this->msg = '默认值 数量不能大于最大值';
                 return false;
             }
-            if($value && $this->field['min'] && count($value)< $this->field['min']){
+            if ($value && $this->field['min'] && count($value) < $this->field['min']) {
                 $this->msg = '默认值 数量不能小于最小值';
+                return false;
+            }
+            //最小值有时,可选项有时，可选项的个数必须大于等于最小值
+            if ($this->field['min'] && $this->field['options'] && count($this->field['options']) < $this->field['min']) {
+                $this->msg = '可选项 个数不能小于最小值';
                 return false;
             }
         } else {
