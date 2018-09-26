8997cbf8b0c8037309873d70b51b9637906995c9
1032380143 <1032380143@qq.com>
2018-09-26 14:24:25 +0800

    后台：表单测试

--- app/Http/Controllers/Api/Admin/FormController.php
+++ app/Http/Controllers/Api/Admin/FormController.php
@@ -37,6 +37,7 @@ class FormController extends Controller
         $rules = $this->formValidator->rules($request);
         $message = $this->formValidator->message();
         $this->validate($request,$rules,[],$message);
+        dd(234);
         $data = $this->formService->store($request);
         return $this->response->post($data);
     }
