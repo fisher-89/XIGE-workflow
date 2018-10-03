e034e4064b0fc8a7fe4c8c827f924cb3fd830cf8
1032380143 <1032380143@qq.com>
2018-10-03 15:11:30 +0800

    前台：发起列表添加form_data数据

--- app/Repository/Web/FlowRunRepository.php
+++ app/Repository/Web/FlowRunRepository.php
@@ -12,6 +12,7 @@ namespace App\Repository\Web;
 use App\Models\FlowRun;
 use App\Models\StepRun;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class FlowRunRepository
 {
@@ -31,7 +32,7 @@ class FlowRunRepository
         $status = $this->status($request->type);
         $data = FlowRun::where('creator_sn', $this->staffSn)
             //筛选流程状态
-            ->whereIn('status',$status)
+            ->whereIn('status', $status)
             //筛选流程
             ->when($request->has('flow_id') && intval($request->flow_id), function ($query) use ($request) {
                 return $query->where('flow_id', $request->flow_id);
@@ -39,7 +40,37 @@ class FlowRunRepository
             ->filterByQueryString()
             ->sortByQueryString()
             ->withPagination();
+        //添加form_data数据
+        if (array_has($data, 'data')) {
+            $data['data'] = $this->getSponsorFormData(collect($data['data']));
+        } else {
+            $data = $this->getSponsorFormData($data);
+        }
         return $data;
+    }
+
+    /**
+     * 获取发起列表的表单data
+     * @param $flowRunData
+     * @return array
+     */
+    protected function getSponsorFormData($flowRunData)
+    {
+        return $flowRunData->map(function ($flowRun) {
+            $tableName = 'form_data_' . $flowRun->form_id;
+            $formData = (array)DB::table($tableName)->where('run_id', $flowRun->id)->first();
+            //表单字段json转数组
+            $formData = array_map(function ($value) {
+                $newValue = json_decode($value, true);
+                if (is_array($newValue) && !is_null($newValue)) {
+                    $value = $newValue;
+                }
+                return $value;
+            }, $formData);
+
+            $flowRun->form_data = $formData;
+            return $flowRun;
+        });
     }
 
     /**
@@ -48,11 +79,12 @@ class FlowRunRepository
      */
     public function getSponsorDetail($flowRunId)
     {
-        $stepRun = StepRun::where(['flow_run_id' => $flowRunId, 'approver_sn' => $this->staffSn, 'action_type' => 1])->orderBy('id','desc')->first();
+        $stepRun = StepRun::where(['flow_run_id' => $flowRunId, 'approver_sn' => $this->staffSn, 'action_type' => 1])->orderBy('id', 'desc')->first();
         $stepRunRepository = new StepRunRepository();
         $data = $stepRunRepository->getDetail($stepRun);
         return $data;
     }
+
     /**
      * 处理发起分类
      * @param $type
@@ -61,7 +93,7 @@ class FlowRunRepository
     {
         switch ($type) {
             case 'all'://全部
-                $status = [0,1,-2,-1];
+                $status = [0, 1, -2, -1];
                 break;
             case 'processing'://进行中
                 $status = [0];
