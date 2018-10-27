1a998ceee7b42ae191023f03d57bfbba93070964
1032380143 <1032380143@qq.com>
2018-10-27 13:32:52 +0800

    前台：流程图功能优化

--- app/Repository/Web/Chart/FlowStepChartRepository.php
+++ app/Repository/Web/Chart/FlowStepChartRepository.php
@@ -11,7 +11,6 @@
 namespace App\Repository\Web\Chart;
 
 
-use App\Models\Step;
 use App\Models\StepRun;
 use Illuminate\Support\Facades\Auth;
 
@@ -22,31 +21,14 @@ class FlowStepChartRepository
         $currentStepRunData = StepRun::where('approver_sn',Auth::id())->findOrFail($stepRunId);
         $allStepRun = StepRun::where('flow_run_id',$currentStepRunData->flow_run_id)
             ->where('action_type','<>',-3)
+            ->select('id','step_id','step_key','approver_sn','approver_name','action_type','next_id','prev_id','acted_at')
             ->orderBy('acted_at','asc')
             ->get();
         //未处理的
         $pendingData = $allStepRun->where('acted_at',null)->pluck([]);
         //已处理完成的
         $finishedData = $allStepRun->whereNotIn('acted_at',[null])->pluck([]);
-        $finishedData = $finishedData->map(function($stepRun)use($allStepRun){
-            $stepRun = $this->getStepRunData($stepRun);
-            return $stepRun;
-        });
-
-        $pendingData = $pendingData->map(function($stepRun)use($allStepRun){
-            $stepRun = $this->getStepRunData($stepRun);
-            return $stepRun;
-        });
-
-        return array_collapse([$finishedData,$pendingData]);
+        $data = array_collapse([$finishedData,$pendingData]);
+        return $data;
     }
-
-    protected function getStepRunData($stepRun)
-    {
-        $stepRun->next = $stepRun->next_id;
-        $stepRun->prev = $stepRun->prev_id;
-        $stepRun = $stepRun->only(['id','step_id','step_key','approver_sn','approver_name','action_type','next','prev','acted_at']);
-        return $stepRun;
-    }
-
 }
