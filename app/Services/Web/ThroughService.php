244baae58041dd71b81f1efa9884063026c4100e
1032380143 <1032380143@qq.com>
2018-10-31 16:39:01 +0800

    前台：通过处理并发合并必须时，next_id添加数据

--- app/Services/Web/ThroughService.php
+++ app/Services/Web/ThroughService.php
@@ -184,6 +184,15 @@ class ThroughService
                 $nextStepRunData = $this->createNextStepRunData($request->input('next_step'));
                 $this->stepRun->next_id = $nextStepRunData->pluck('id')->all();
                 $this->stepRun->save();
+
+                //并发合并必须时，修改并发步骤的的next_id 为最后提交的
+                if($this->stepRun->steps->merge_type == 1 && $this->stepRun->action_type == 2){
+                    $prevStepKeys = $this->stepRun->steps->prev_step_key;
+                    $prevStepIds= Step::where('flow_id' , $this->stepRun->flow_id)->whereIn('step_key',$prevStepKeys)->pluck('id')->all();
+                    StepRun::where(['flow_id'=>$this->stepRun->flow_id,'flow_run_id'=>$this->stepRun->flow_run_id,'next_id'=>'[]'])
+                        ->whereIn('step_id',$prevStepIds)
+                        ->update(['next_id'=>json_encode([$this->stepRun->id])]);
+                }
             }
 
         }
