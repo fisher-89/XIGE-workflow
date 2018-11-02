316b6d6578ec862e10be5410ce696f6ab6b3993b
1032380143 <1032380143@qq.com>
2018-11-02 16:55:06 +0800

    前台：抄送人添加发送工作通知功能，工作通知发送审批人的data优化

--- app/Services/Notification/Message.php
+++ app/Services/Notification/Message.php
@@ -9,6 +9,7 @@
 namespace App\Services\Notification;
 
 
+use App\Models\StepCc;
 use App\Services\OA\OaApiService;
 
 trait Message
@@ -30,28 +31,62 @@ trait Message
         } else {
             $url = $url . '/' . $stepRun->id;
         }
+        $title = $stepRun->flowRun->creator_name . '发起的' . $stepRun->flow_name . '需要你审批';
+        $data = $this->getSendData($stepRun->id,[$stepRun->approver_id],$url,$title,$topThreeFormData);
+//        $data = [
+//            'step_run_id' => $stepRun->id,
+//            'oa_client_id' => config('oa.client_id'),
+//            'userid_list' => [$stepRun->approver_sn],
+//            'msg' => [
+//                'msgtype' => 'oa',
+//                'oa' => [
+//                    'message_url' => $url,
+//                    'head' => [
+//                        'bgcolor' => 'FFF44336',
+//                        'text' => '工作流'
+//                    ],
+//                    'body' => [
+//                        'title' => $stepRun->flowRun->creator_name . '发起的' . $stepRun->flow_name . '需要你审批',
+//                        'form' => $topThreeFormData
+//                    ]
+//                ]
+//            ]
+//        ];
+        return $this->sendToOaApi($data);
+    }
+
+    /**
+     * 获取发送的data
+     * @param int $stepRunId
+     * @param array $userList
+     * @param string $messageUrl
+     * @param string $title
+     * @param array $formData
+     * @return array
+     */
+    protected function getSendData(int $stepRunId,array $userList,string $messageUrl,string $title,array $formData)
+    {
         $data = [
-            'step_run_id' => $stepRun->id,
+            'step_run_id' => $stepRunId,
             'oa_client_id' => config('oa.client_id'),
-            'userid_list' => [$stepRun->approver_sn],
+            'userid_list' => $userList,
             'msg' => [
                 'msgtype' => 'oa',
                 'oa' => [
-                    'message_url' => $url,
+                    'message_url' => $messageUrl,
                     'head' => [
                         'bgcolor' => 'FFF44336',
                         'text' => '工作流'
                     ],
                     'body' => [
-                        'title' => $stepRun->flowRun->creator_name . '发起的' . $stepRun->flow_name . '需要你审批',
-                        'form' => $topThreeFormData
+                        'title' => $title,
+                        'form' => $formData
                     ]
                 ]
             ]
         ];
-        return $this->sendToOaApi($data);
+        return $data;
     }
-
     /**
      * text 工作通知
      * @param $stepRun
@@ -71,6 +106,31 @@ trait Message
                 ]
             ]
         ];
+        return $this->sendToOaApi($data);
+    }
+
+    /**
+     * 发送抄送人OA工作通知信息
+     * @param $stepRun
+     * @param array $formData
+     * @param int $staffSn
+     * @return mixed
+     */
+    public function sendCcJobOaMessage($stepRun,array $formData,int $staffSn)
+    {
+        //前三表单data
+        $topThreeFormData = $this->getTopThreeFormData($formData, $stepRun->form_id);
+        $stepCc = StepCc::where(['step_run_id'=>$stepRun->id,'staff_sn'=>$staffSn])->first();
+        $url = request()->get('cc_host');
+        if (str_is('*cclist?source=dingtalk', $url)) {
+            $arr = explode('?', $url);
+            $url = $arr[0] . '/' . $stepCc->id . '?' . $arr[1];
+        } else {
+            $url = $url . '/' . $stepCc->id;
+        }
+        $title = $stepRun->flowRun->creator_name . '发起的' . $stepRun->flow_name . '的流程下'.$stepRun->step_name.'的步骤的数据，抄送给你了';
+
+        $data = $this->getSendData($stepRun->id,[$staffSn],$url,$title,$topThreeFormData);
         return $this->sendToOaApi($data);
     }
 
