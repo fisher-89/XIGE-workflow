f9cd853772d0e66a59e6d95d183625182b4e8749
1032380143 <1032380143@qq.com>
2018-11-03 11:28:49 +0800

    前台：转交捕获异常添加到更新待办里

--- app/Services/Web/DeliverService.php
+++ app/Services/Web/DeliverService.php
@@ -53,17 +53,18 @@ class DeliverService
             $stepRun->save();
 
             //触发转交回调
-            $this->sendCallback->sendCallback($deliverData->id,'step_deliver');
+            $this->sendCallback->sendCallback($deliverData->id, 'step_deliver');
 
-            //更新待办
-            $dingTalkMessage = new MessageNotification();
-            $updateTodoResult = $dingTalkMessage->updateTodo($stepRun->id);
-            abort_if($updateTodoResult == 0,400,'发送更新待办通知失败');
+            try {
+                //更新待办
+                $dingTalkMessage = new MessageNotification();
+                $updateTodoResult = $dingTalkMessage->updateTodo($stepRun->id);
+                abort_if($updateTodoResult == 0, 400, '发送更新待办通知失败');
 
-            //发送钉钉消息（发送给下一步审批人）
-            try{
-                $this->sendMessage($deliverData,$stepRun->approver_name);
-            }catch (\Exception $e){
+                //发送钉钉消息（发送给下一步审批人）
+
+                $this->sendMessage($deliverData, $stepRun->approver_name);
+            } catch (\Exception $e) {
 
             }
 
@@ -75,7 +76,7 @@ class DeliverService
      * 发送消息
      * @param $stepRun
      */
-    protected function sendMessage($stepRun,string $approverName)
+    protected function sendMessage($stepRun, string $approverName)
     {
         //表单Data
         $formData = $this->formRepository->getFormData($stepRun->flow_run_id);
@@ -88,7 +89,7 @@ class DeliverService
             //流程允许发送通知
 
             //发送通知给审批人
-            if($stepRun->steps->send_todo){
+            if ($stepRun->steps->send_todo) {
                 //允许发送待办通知
                 $todoResult = $this->dingTalkMessage->sendTodoMessage($stepRun, $formData);
                 abort_if($todoResult == 0, 400, '发送待办通知失败');
@@ -98,9 +99,9 @@ class DeliverService
             }
 
             //发送工作通知text消息 给发起人
-            if($stepRun->steps->send_start){
-                $content = '你发起的'.$stepRun->flow_name.'流程被'.$approverName.'转交给'.$stepRun->approver_name.'审批了';
-                $result = $this->dingTalkMessage->sendJobTextMessage($stepRun,$content);
+            if ($stepRun->steps->send_start) {
+                $content = '你发起的' . $stepRun->flow_name . '流程被' . $approverName . '转交给' . $stepRun->approver_name . '审批了';
+                $result = $this->dingTalkMessage->sendJobTextMessage($stepRun, $content);
                 abort_if($result == 0, 400, '发送工作通知失败');
             }
 
