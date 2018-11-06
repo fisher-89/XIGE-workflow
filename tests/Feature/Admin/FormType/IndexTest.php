0920d171e384163c39bc2359c748ad374142889f
1032380143 <1032380143@qq.com>
2018-11-06 16:45:13 +0800

    后台：测试添加表单分类列表

---
+++ tests/Feature/Admin/FormType/IndexTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature\Admin\FormType;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class IndexTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $response = $this->get('/admin/form-type');
+        $response->assertStatus(200);
+//        $this->assertTrue(true);
+    }
+}
