1664a74bcdb1b5dbfe3909567f07a1e8ae2c6cbb
1032380143 <1032380143@qq.com>
2018-11-07 17:36:00 +0800

    测试：开始

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,19 @@
 
 namespace Tests;
 
+use App\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * 认证API
+     */
+    public function apiAuth()
+    {
+        $user = factory(User::class)->create();
+        $this->actingAs($user, 'api');
+    }
 }
