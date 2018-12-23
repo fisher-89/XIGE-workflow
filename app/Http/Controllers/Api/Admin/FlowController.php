0f00f6f1b4a8c4134df7ae1fe6115e6bdacbaa57
1032380143 <1032380143@qq.com>
2018-12-23 14:43:29 +0800

    后台：流程列表接口去掉步骤关联

--- app/Http/Controllers/Api/Admin/FlowController.php
+++ app/Http/Controllers/Api/Admin/FlowController.php
@@ -55,16 +55,17 @@ class FlowController extends Controller
      */
     public function index()
     {
-        $query = Flow::with('steps');
         //超级管理员
         $super = $this->role->getSuperStaff();
         $flowNumber = $this->role->getFlowNumber();
 
         if (empty($super) || ($super && (!in_array(Auth::id(), $super)))) {
             //没有超级管理员 或 有超级管理员 并且不在超级管理员中
-            $query = $query->whereIn('number', $flowNumber);
+            $data = $query = Flow::whereIn('number', $flowNumber)->orderBy('sort', 'asc')->get();
+        } else {
+            $data = Flow::orderBy('sort', 'asc')->get();
         }
-        $data = $query->orderBy('sort', 'asc')->get();
+
         return $this->response->get($data);
     }
 
